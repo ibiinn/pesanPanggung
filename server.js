ca58a42334b90bc3cb3ba9e74e211903b55c5c5e
ibiin <117567744+ibiin@users.noreply.github.com>
2026-02-12 04:24:55 +0700

    konfig server

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
+app.listen(port);
+
 let messageHistory = [];
 
 app.use(express.static(path.join(__dirname, 'public')));
