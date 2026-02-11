0f37bf913ae5813f1c9c10e35bb53f646970c6f7
ibiin <117567744+ibiin@users.noreply.github.com>
2026-02-12 05:46:48 +0700

    fix vercel routing & public path

--- server.js
+++ server.js
@@ -6,41 +6,6 @@ const path = require('path');
 
 const PORT = process.env.PORT || 4000;
 
-let messageHistory = [];
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/send', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'send.html'));
-});
-
-io.on('connection', (socket) => {
-    console.log('User connected');
-    socket.emit('init_messages', messageHistory);
-
-    socket.on('new_message', (data) => {
-        // Sanitasi input biar gak kena hack XSS
-        const safeUser = (data.username || 'Anonim').replace(/</g, "&lt;").replace(/>/g, "&gt;");
-        const safeText = data.text.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-        
-        const messageData = {
-            username: safeUser,
-            text: safeText,
-            time: new Date().toLocaleTimeString('id-ID', { hour: '2-digit', minute: '2-digit' })
-        };
-
-        messageHistory.push(messageData);
-        // Simpan 5 pesan terakhir
-        if (messageHistory.length > 7) messageHistory.shift();
-        
-        io.emit('update_messages', messageHistory);
-    });
-});
-
 http.listen(PORT, () => {
     console.log(`Server jalan di http://localhost:${PORT}`);
 });
